de87568ed31943f1ef98d72f1b91374dd179bf44
Renz-Eryll <you@example.com>
2025-06-09 11:33:09 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>MOVIE APP</h1>
+          </header>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
